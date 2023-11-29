8bbee8c9a2e76a9e12bc041364d91851a5446ccb
LucasBaccaro <68014426+LucasBaccaro@users.noreply.github.com>
2023-11-29 17:39:59 -0300

    Revert "Install moko resources"

--- shared/src/commonMain/kotlin/App.kt
+++ shared/src/commonMain/kotlin/App.kt
@@ -12,8 +12,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import com.myapplication.common.MR
-import dev.icerock.moko.resources.compose.stringResource
 import org.jetbrains.compose.resources.ExperimentalResourceApi
 import org.jetbrains.compose.resources.painterResource
 
@@ -30,7 +28,12 @@ fun App() {
             }) {
                 Text(greetingText)
             }
-            Text(stringResource(MR.strings.hello))
+            AnimatedVisibility(showImage) {
+                Image(
+                    painterResource("compose-multiplatform.xml"),
+                    contentDescription = "Compose Multiplatform icon"
+                )
+            }
         }
     }
 }
